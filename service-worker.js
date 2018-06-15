2fdc30a8c90eb1c9c0ea460d545efe3a5dd7ec12
tshimizu09 <tshimizu09@gmail.com>
2018-06-16 07:47:59 +0900

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-# service-worker.js
+// service-worker.js
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
 });
@@ -6,3 +6,6 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
+
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+self.addEventListener('fetch', function(event) {});
